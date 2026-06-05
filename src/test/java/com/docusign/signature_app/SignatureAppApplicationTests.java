bb0a3641a72dfd2ac11d2809fd201e4de1944bbf
Mohammed-Anas0617 <mohammedanas0617@gmail.com>
2026-06-05 19:51:44 +0530

    Day 1: Initial Spring Boot project setup

---
+++ src/test/java/com/docusign/signature_app/SignatureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docusign.signature_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignatureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
